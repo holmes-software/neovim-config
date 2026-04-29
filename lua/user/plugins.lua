88d0fcd325cf58dc9ab54a19b01e7126ac5e345d
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2026-04-29 13:32:09 -0700

    Remove opencode

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -191,20 +191,6 @@ require("lazy").setup({
         end
     },
     { "nvim-telescope/telescope-dap.nvim" },
-
-    --AI
-    {
-      "nickjvandyke/opencode.nvim",
-      dependencies = {
-        -- Recommended for `ask()` and `select()`.
-        -- Required for `snacks` provider.
-        ---@module 'snacks' <- Loads `snacks.nvim` types for configuration intellisense.
-        { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
-      },
-      config = function()
-          require("user.opencode")
-      end
-    }
 }, {
     ui = {
         border = "rounded",
